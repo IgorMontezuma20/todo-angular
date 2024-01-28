e68dd237e15f0a3d2456ce0f178d7823ffa0dade
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-28 17:51:38 -0300

    feat: Persistindo dados no Local Storage

--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -21,17 +21,17 @@ export class InputAddItemComponent {
 
   @ViewChild('inputText') public inputText!: ElementRef;
 
-  @Output() public outputListItems = new EventEmitter<IListItems>();
+  @Output() public outputAddListItem = new EventEmitter<IListItems>();
   public focusAndAddItem(value: string) {
     if (value) {
       this.#cdr.detectChanges();
       this.inputText.nativeElement.value = '';
 
-      const actualDate = new Date();
-      const timestamp = actualDate.getTime();
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
       const id = `ID-${timestamp}`;
 
-      this.outputListItems.emit({
+      this.outputAddListItem.emit({
         id,
         checked: false,
         value,
