279ea213b16d3b397b286b6613d6265a50b841d2
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-28 21:19:27 -0300

    feat: Implementando o List Component

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
 import { IListItems } from '../../interface/IListItems.interface';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [InputAddItemComponent],
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -28,7 +29,7 @@ export class ListComponent {
     return this.#setListItems.set(this.#parseItems());
   }
 
-  public deleteAllItems(){
+  public deleteAllItems() {
     localStorage.removeItem('@my-list');
     return this.#setListItems.set(this.#parseItems());
   }
